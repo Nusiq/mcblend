7e1ffdc99140a754f00ed35674a28e417b08a539
Nusiq <nusiq93@gmail.com>
2020-04-21 16:37:38 +0200

    Added basic setup for testing on github

--- blender_scripts/install.py
+++ blender_scripts/install.py
@@ -16,8 +16,12 @@ def main(zip_path):
     '''
     - zip_path - absolute path to zip file with mcblend.
     '''
+    print(f'Installing the addon from {zip_path}')
     bpy.ops.preferences.addon_install(filepath=zip_path)
+
+    print(f'Enabling module "mcblend"')
     bpy.ops.preferences.addon_enable(module="mcblend")
+    bpy.ops.wm.save_userpref()
 
 if __name__ == "__main__":
     main(argv[0])
