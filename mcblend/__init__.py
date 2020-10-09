17a9e285d6ce1e6ec501a86fcaa7035c7b915b8d
Nusiq <>
2020-10-09 13:40:56 +0200

    Updated version number

--- mcblend/__init__.py
+++ mcblend/__init__.py
@@ -66,7 +66,7 @@ bl_info = {
     "author": "Artur",
     "description": "",
     "blender": (2, 80, 0),
-    "version": (4, 0, 1),  # COMPATIBILITY BREAKING CHANGE, NEW FEATURE, BUGFIX
+    "version": (5, 0, 0),  # COMPATIBILITY BREAKING CHANGE, NEW FEATURE, BUGFIX
     "location": "",
     "warning": "",
     "category": "Generic"
