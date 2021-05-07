336a297c59c83adcfb91a28336ca28f3fb14eea8
Nusiq <>
2021-05-07 14:24:33 +0200

    Added test for issue #89 (uv-mapping test)

---
+++ blender_scripts/uv_map_and_export_model.py
@@ -0,0 +1,25 @@
+'''
+Select scene passed in commandline arguments, select all, export animation
+to path passed in arguments.
+
+This script should be executed after opening testing file with a model that
+have animation.
+'''
+import sys
+import bpy
+
+
+# Collect arguments after "--"
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]
+
+
+def main(scene_name: str, target_path: str):
+    '''Main function.'''
+    bpy.context.window.scene = bpy.data.scenes[scene_name]
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.nusiq_mcblend.map_uv()
+    bpy.ops.nusiq_mcblend.export_model(filepath=target_path)
+
+if __name__ == "__main__":
+    main(argv[0], argv[1])
---
+++ tests/test_uv_mapper.py
@@ -0,0 +1,91 @@
+'''
+This is a testing script for UV-mapping and exporting models. Applies the
+UV-mapping to models from the list, than exports them from blend file and
+compares them with the expected result.
+'''
+import os
+import shutil
+import json
+from pathlib import Path
+import typing as tp
+
+import pytest
+from .common import assert_is_model, blender_run_script, compare_json_files
+
+
+def make_comparison_files(
+        tmp: str, scene_name: str, blend_file_path: str,
+    ) -> tp.Tuple[tp.Dict, tp.Dict]:
+    '''
+    Opens blender file, selects scene, maps UV, and exports model from that to
+    given tmp path.
+
+    Returns the result JSON in a dictionary and the path to newly created file.
+    '''
+    tmp = os.path.abspath(tmp)
+    target = os.path.join(tmp, f'{scene_name}.geo.json')
+    expected_result_path = (
+        f'./tests/data/test_uv_mapper/{scene_name}.geo.json'
+    )
+
+    script = os.path.abspath('./blender_scripts/uv_map_and_export_model.py')
+    blend_file_path = os.path.abspath(blend_file_path)
+
+    # Windows uses wierd path separators
+    tmp = tmp.replace('\\', '/')
+    target = target.replace('\\', '/')
+    script = script.replace('\\', '/')
+
+
+    # Create tmp if not exists
+    Path(tmp).mkdir(parents=True, exist_ok=True)
+
+    # Run blender actions
+    blender_run_script(
+        script, scene_name, target, blend_file_path=blend_file_path
+    )
+
+    # Return results
+    with open(target, 'r') as f:
+        target_dict = json.load(f)
+    with open(expected_result_path, 'r') as f:
+        expected_result = json.load(f)
+
+    return target_dict, expected_result  # type: ignore
+
+# PYTEST FUNCTIONS
+SCENES = [
+    'issue89'
+]
+
+
+def setup_module(module):
+    '''Runs before tests'''
+    tmp_path = "./.tmp/test_uv_mapper"
+    if os.path.exists(tmp_path):
+        shutil.rmtree(tmp_path)
+
+
+@pytest.fixture(params=SCENES)
+def scene(request):
+    return request.param
+
+
+# TESTS
+def test_importer(scene):
+    result, expected_result = make_comparison_files(
+        "./.tmp/test_uv_mapper", scene,
+        './tests/data/tests_project.blend'
+    )
+
+    assert_is_model(result)
+    set_paths = {
+        ("minecraft:geometry"),
+        ("minecraft:geometry", int, "bones"),
+        ("minecraft:geometry", int, "bones", int, "cubes"),
+        ("minecraft:geometry", int, "bones", int, "cubes", int, "locators"),
+    }
+
+    compare_json_files(
+        expected_result, result, atol=0.01,
+        ignore_order_paths=set_paths)
