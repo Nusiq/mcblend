210417076d4ae113bb19d72853b68bff01282d0e
Nusiq <nusiq@outlook.com>
2022-09-25 18:22:14 +0200

    v9.4.0 updated version number.

--- mcblend/__init__.py
+++ mcblend/__init__.py
@@ -114,7 +114,7 @@ bl_info = {
     "author": "Artur",
     "description": "Minecraft Bedrock Edition addon for creating entity models and animations.",
     "blender": (2, 80, 0),
-    "version": (9, 3, 7),  # COMPATIBILITY BREAKING CHANGE, NEW FEATURE, BUGFIX
+    "version": (9, 4, 0),  # COMPATIBILITY BREAKING CHANGE, NEW FEATURE, BUGFIX
     "location": "",
     "warning": "",
     "category": "Object"
