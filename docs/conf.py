94462cf9a36ac0e9d81f325be5430dff1d6160d7
Nusiq <nusiq@outlook.com>
2026-06-12 12:52:23 +0200

    Updated the version number to 12.1.0.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'mcblend'
 copyright = '2022, Nusiq'
 author = 'Nusiq'
-release = '12.0.0'
+release = '12.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
